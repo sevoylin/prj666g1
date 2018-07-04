fa656356e66e37f8f0c38fda70c1f5e173e4ad1a
ytian38 <31732714+ytian38@users.noreply.github.com>
2018-07-04 15:57:09 -0400

    change main page to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import firebase from 'firebase';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
